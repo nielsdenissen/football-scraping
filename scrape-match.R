92e0a908c497b84d1483e360fb29dc9b013d62fd
Niels Denissen <niels.denissen@ing.nl>
2016-01-26 15:16:42 +0100

    adjusted gathering of data in flexible way

--- scrape-match.R
+++ scrape-match.R
@@ -6,36 +6,59 @@ library(stringr)
 library(dplyr)
 library(ggplot2)
 library(GGally)
-
-setwd("~/accellerator/flight-ticket-scraping/")
+library(testthat)
 
 scrape_match <- function(url) {
 
   football <- read_html(url)
-  details <- football %>% html_nodes("dl")
-  referees <- details[4] %>% html_nodes("dd a")
   content <- football %>% html_nodes("#page_match_1_block_match_info_4")
   
   # Warning: The order of elements might change 
   # content %>% html_nodes("dt") %>% html_text
   
   match_details <- content %>% html_nodes("dd") %>% html_text
+  match_details_headers <- content %>% html_nodes("dt") %>% html_text
   
-  data.frame (
-    team_home = content %>% html_nodes(".container.left h3") %>% html_text,
-    team_away = content %>% html_nodes(".container.right h3") %>% html_text,
-    result = content %>% html_nodes(".thick.scoretime") %>% html_text,
-    competition = match_details[1],
-    date = match_details[2],
-    game_week = match_details[3],
-    kick_off = match_details[4],
-    half_time = match_details[5],
-    full_time = match_details[6],
-    venue = match_details[7],
-    referee_name = referees[1] %>% html_text,
-    referee_href = referees[1] %>% html_attr("href")
-  )  %>% lapply(str_trim)
-
+  
+  details <- football %>% html_nodes("dl")
+  referees <- tryCatch({details[4] %>% html_nodes("dd a")},error=function(err){return(NA)})
+  referees_header <- tryCatch({details[4] %>% html_nodes("dt")},error=function(err){return(NA)})
+  
+  test <- list()
+  for(i in 1:length(match_details)){
+    test[[match_details_headers[i]]] <- match_details[i]
+  }
+  
+  if(!is.na(referees[1])){
+    referee_name <- referees[1] %>% html_text
+    referee_header <- referees_header[1] %>% html_text
+    test[[referee_header]] <- referee_name
+  }
+  
+  test %>% lapply(str_trim)
 }
 
-match <- scrape_match("http://int.soccerway.com/matches/2016/01/16/italy/serie-a/atalanta-bergamo/fc-internazionale-milano/2120570/?ICID=PL_MS_01")
+scrape_match_by_number <- function(number) {
+  scrape_match(paste0("http://int.soccerway.com/matches/9999/99/99/some-league/serie-a/team1/team2/", number))
+}
+
+
+df <- data.frame()
+for (i in 2120375:2120000) {
+  print(paste("get match", i))
+  match <- scrape_match_by_number(i)
+  dftest <- data.frame(match)
+  df <- bind_rows(df,dftest)
+}
+
+
+
+
+
+
+# test_that("team home is a character array", {
+#   expect_equal(is.character(match$team_home), TRUE)
+#   expect_equal(is.character(match$team_away), TRUE)
+#   expect_equal(is.character(match$result), TRUE)
+# })
+
